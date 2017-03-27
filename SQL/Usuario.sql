403edb986864abacf798f48cb7f9b6c1b76e461b
Melchor Garau Madrigal <melchor9000@gmail.com>
2017-03-27 11:17:38 +0200

    Añadir el contenido de Usuario.sql

--- SQL/Usuario.sql
+++ SQL/Usuario.sql
@@ -1,1 +1,5 @@
-
+CREATE USER IF NOT EXISTS tecnoweb@localhost IDENTIFIED BY 'reflexiona';
+GRANT SELECT on tecnoweb.* TO tecnoweb@localhost;
+GRANT INSERT on tecnoweb.* TO tecnoweb@localhost;
+GRANT DELETE on tecnoweb.* TO tecnoweb@localhost;
+GRANT UPDATE on tecnoweb.* TO tecnoweb@localhost;
