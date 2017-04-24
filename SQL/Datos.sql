dae82d7c66bda7abbd63f46bd37e26463cfe2c72
Melchor Garau Madrigal <melchor9000@gmail.com>
2017-04-24 11:24:04 +0200

    Arreglo de contraseñas en datos

--- SQL/Datos.sql
+++ SQL/Datos.sql
@@ -2,13 +2,13 @@ SET NAMES 'utf8mb4';
 
 INSERT INTO `Usuario` (`id`, `Nombre`, `Apellidos`, `Twitter`, `Instagram`, `Web`, `Foto`, `NombreUsuario`, `Contraseña`, `Correo`)
 VALUES
-    (1, 'Manolito', 'Casual', 'ManuelNotV', 'manuelnotv', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/14592033_1468653206484196_4673932653030801408_a.jpg', 'manuelnotv', 'd8c61debc1ce96f6cfcc809c366eb844510f976b3353913f51e0aca0d8afc833', 'veredasmanuel@gmail.com'),
-    (2, 'Jose', 'Pato', NULL, NULL, NULL, NULL, 'jose', 'f06aa1a6bd55c88b4f7f0db42eccb2ca5ca62d3af9c74647b4ce9d37b6386aa4', 'jose_mj96@hotmail.com'),
-    (3, 'Antonio Angel', 'Cruzado Castillo', NULL, 'a.angelcc', 'http://www.antonioangel.xyz', 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/12519652_920506371400433_502767576_a.jpg', 'a.angelcc', '4e9fc9ab1aa53f31c312648f11a61455436897f5eb6348b1d240f9dd36fbdf07', 'a.angelcruzado@gmail.com'),
-    (4, 'Eduardo', 'Guidet', 'EduardoDobleH', 'eduardodobleh', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/13437174_1621256951521609_1330748942_a.jpg', 'eduardodobleh', '8b932958ace3766bfb830efa7729b92e5566daf9b2a959ecdb6ad85196b26248', 'eduardogj96@gmail.com'),
-    (5, 'Melchor Alejo', 'Garau Madrigal', 'Melchor629', 'melchor9000', 'http://melchor9000.me', 'https://pbs.twimg.com/profile_images/842841793842634752/mHCFmjI5.jpg', 'melchor9000', 'b810896d4fb913646c5dc6b02d3bc231972dbd899c6d1a5ccda0d347763da326', 'melchor9000@gmail.com'),
-    (6, 'Rodrigo', 'Represa Represa', NULL, 'rodri_rr', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/10809643_574618769335088_1009160558_a.jpg', 'rodri_rr', 'ee5234a9bb638d210366bbb7e8fe266fe76bb52aee8498fbfb5ba321a2788ef9', 'rodrigorepresa@gmail.com'),
-    (7, 'Francis', 'Reyes Sánchez', 'francis1196', 'francis8rs', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/17267585_781564748686947_637854320502505472_a.jpg', 'francis1196', 'd942b481e68f77344cc4ce10e991861560f342f0c7b270fbc13d066be72c8fd5', 'francis8rs@gmail.com');
+    (1, 'Manolito', 'Casual', 'ManuelNotV', 'manuelnotv', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/14592033_1468653206484196_4673932653030801408_a.jpg', 'manuelnotv', '44b7cf02cf5b79b9a85914fbad2ff6d53bb134fdf2b8d1c93f68f88c53f0d478', 'veredasmanuel@gmail.com'),
+    (2, 'Jose', 'Pato', NULL, NULL, NULL, NULL, 'jose', 'bbe3e80d04d1213630d3ccf6786f7c38b7f3aa90152ae0c0004db827b2b72b03', 'jose_mj96@hotmail.com'),
+    (3, 'Antonio Angel', 'Cruzado Castillo', NULL, 'a.angelcc', 'http://www.antonioangel.xyz', 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/12519652_920506371400433_502767576_a.jpg', 'a.angelcc', 'c7852be47c1c5e6d81456da6aed1336e1d65c1ad31429dc7b986276489c544f7', 'a.angelcruzado@gmail.com'),
+    (4, 'Eduardo', 'Guidet', 'EduardoDobleH', 'eduardodobleh', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/13437174_1621256951521609_1330748942_a.jpg', 'eduardodobleh', 'b0034b9d5b05152488f834aef3b5607dd28d4526e3d0c9b33c851bc45151984e', 'eduardogj96@gmail.com'),
+    (5, 'Melchor Alejo', 'Garau Madrigal', 'Melchor629', 'melchor9000', 'http://melchor9000.me', 'https://pbs.twimg.com/profile_images/842841793842634752/mHCFmjI5.jpg', 'melchor9000', '6bdbaaefbd6324af7a2e799f3d7a0c06c880981b08ebf9c6dec49b6ec7353df7', 'melchor9000@gmail.com'),
+    (6, 'Rodrigo', 'Represa Represa', NULL, 'rodri_rr', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/10809643_574618769335088_1009160558_a.jpg', 'rodri_rr', '9c8da44b6db95e30d04f2fefa4b336a1014e37afda8b895f3bd5b2c467ad66db', 'rodrigorepresa@gmail.com'),
+    (7, 'Francis', 'Reyes Sánchez', 'francis1196', 'francis8rs', NULL, 'https://scontent-mad1-1.cdninstagram.com/t51.2885-19/s320x320/17267585_781564748686947_637854320502505472_a.jpg', 'francis1196', 'aa5d2b96985a8e1d6747e733d83bdf7bdd96a615a5b598c438d0a730ded54dad', 'francis8rs@gmail.com');
 
 INSERT INTO `Aficiones` (`idUsuario`, `Nombre`)
 VALUES
