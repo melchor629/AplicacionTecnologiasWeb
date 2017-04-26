7832d668758347b88aef2f2a4884e63fc8eb85f4
Antonio Angel <a.angelcruzado@gmail.com>
2017-04-26 12:39:36 +0200

    Implementacion metodos en facades y creacion de pagina para panel de notificaciones

--- ejb/src/java/app/ejb/PeticionAmistadFacade.java
+++ ejb/src/java/app/ejb/PeticionAmistadFacade.java
@@ -1,6 +1,8 @@
 package app.ejb;
 
 import app.entity.PeticionAmistad;
+import app.entity.PeticionAmistadPK;
+import app.entity.Usuario;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -22,6 +24,26 @@ public class PeticionAmistadFacade extends AbstractFacade<PeticionAmistad> {
 
     public PeticionAmistadFacade() {
         super(PeticionAmistad.class);
+    }
+    
+    // Permite a un usuario mandar una peticion de amistad a otro usuario
+    public void mandarPeticionAmistad(int idDesde, int idHacia, String mensaje){
+        PeticionAmistad peticion = new PeticionAmistad();
+        peticion.setUsuario(getEntityManager().find(Usuario.class, idDesde));
+        peticion.setUsuario(getEntityManager().find(Usuario.class, idHacia));
+        peticion.setMensaje(mensaje);
+        
+        // Se guarda en la base de datos
+        getEntityManager().persist(peticion);
+    }
+    
+    // Permite averiguar si hay una peticion de amistad mandada
+    public void peticionMandada(int idDesde, int idHacia){
+        // Contemplar la antisimetría
+        PeticionAmistadPK clavePrimaria1 = new PeticionAmistadPK(idDesde, idHacia);
+        PeticionAmistadPK clavePrimaria2 = new PeticionAmistadPK(idDesde, idHacia);
+        PeticionAmistad peticion1 = getEntityManager().find(PeticionAmistad.class, clavePrimaria1);
+        PeticionAmistad peticion2 = getEntityManager().find(PeticionAmistad.class, clavePrimaria2);
     }
     
 }
--- ejb/src/java/app/ejb/UsuarioFacade.java
+++ ejb/src/java/app/ejb/UsuarioFacade.java
@@ -1,5 +1,6 @@
 package app.ejb;
 
+import app.entity.PeticionAmistad;
 import app.entity.Usuario;
 import java.util.Collection;
 import java.util.List;
@@ -26,49 +27,60 @@ public class UsuarioFacade extends AbstractFacade<Usuario> {
     public UsuarioFacade() {
         super(Usuario.class);
     }
-    
-    public Usuario obtenerUsuario (String usuario, String pass) {
+
+    public Usuario obtenerUsuario(String usuario, String pass) {
         Query q;
         q = this.em.createQuery("SELECT u FROM Usuario u WHERE u.nombreUsuario = :usuario AND u.contraseña = :password ");
-        String hashPasado = app.cosas.Hash.hash(usuario+":"+pass);
+        String hashPasado = app.cosas.Hash.hash(usuario + ":" + pass);
         q.setParameter("usuario", usuario);
-        q.setParameter("password",hashPasado);
-        List<Usuario> lista = (List)q.getResultList();
+        q.setParameter("password", hashPasado);
+        List<Usuario> lista = (List) q.getResultList();
         System.out.println(pass + " " + hashPasado);
-        
-        if (lista.size()>0){
+
+        if (lista.size() > 0) {
             return lista.get(0);
-        } else{
+        } else {
             return null;
-        }   
-               
+        }
+
     }
-    
-    public Usuario obtenerUsuarioPorId(int idUsuario){
+
+    public Usuario obtenerUsuarioPorId(int idUsuario) {
         EntityManager gestorEntidades = getEntityManager();
-        
-       Usuario usuario = gestorEntidades.find(Usuario.class, idUsuario);
-        
-       /* Query q;
+
+        Usuario usuario = gestorEntidades.find(Usuario.class, idUsuario);
+
+        /* Query q;
         q = gestorEntidades.createQuery("SELECT u FROM Usuario u WHERE u.id = :idUsuario");
         q.setParameter("idUsuario", idUsuario);
         Usuario usuario = (Usuario) q.getSingleResult(); */
-       
         return usuario;
     }
-    
-    public boolean sonAmigos(int id1, int id2){
-       /* Query consulta = getEntityManager().createQuery("SELECT amistad FROM Contactos amistad WHERE (amistad.idUsuario = :id1 AND amistad.Amigo = :id2) OR (amistad.idUsuario = :id2 AND amistad.Amigo = :id1)");
+
+    public boolean sonAmigos(int id1, int id2) {
+        /* Query consulta = getEntityManager().createQuery("SELECT amistad FROM Contactos amistad WHERE (amistad.idUsuario = :id1 AND amistad.Amigo = :id2) OR (amistad.idUsuario = :id2 AND amistad.Amigo = :id1)");
         consulta.setParameter("id1", id1);
         consulta.setParameter("id2", id2);
         int resultados = consulta.getResultList().size(); */
-       Usuario usuario1 = getEntityManager().find(Usuario.class, id1);
-       Usuario usuario2 = getEntityManager().find(Usuario.class, id2);
-       
-       Collection<Usuario> coleccion1 = usuario1.getUsuarioCollection();
-       Collection<Usuario> coleccion2 = usuario1.getUsuarioCollection1();
-       
+        Usuario usuario1 = getEntityManager().find(Usuario.class, id1);
+        Usuario usuario2 = getEntityManager().find(Usuario.class, id2);
+
+        Collection<Usuario> coleccion1 = usuario1.getUsuarioCollection();
+        Collection<Usuario> coleccion2 = usuario1.getUsuarioCollection1();
+
         return coleccion1.contains(usuario2) || coleccion2.contains(usuario2);
     }
-    
+
+    // Le permite al usuario con id idDesde aceptar una peticion de amistad
+    // al usuario idHacia
+    public void aceptarPeticionAmistad(int idDesde, int idHacia) {
+
+        // El usuario que menda la peticion
+        Usuario usuario = getEntityManager().find(Usuario.class, idDesde);
+        Usuario futuraAmistad = getEntityManager().find(Usuario.class, idHacia);
+        // Se añade el usuario a su lista de amigos
+        usuario.getUsuarioCollection().add(futuraAmistad);
+        // Se actualiza la entidad
+        this.edit(usuario);
+    }
 }
