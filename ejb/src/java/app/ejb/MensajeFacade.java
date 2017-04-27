c510febc4db9f85db1288d327b1c8fef767dd5c7
Antonio Angel <a.angelcruzado@gmail.com>
2017-04-27 11:26:45 +0200

    Algunos metodos a MensajesFacade

--- ejb/src/java/app/ejb/MensajeFacade.java
+++ ejb/src/java/app/ejb/MensajeFacade.java
@@ -1,9 +1,11 @@
 package app.ejb;
 
 import app.entity.Mensaje;
+import java.util.Collection;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -24,4 +26,22 @@ public class MensajeFacade extends AbstractFacade<Mensaje> {
         super(Mensaje.class);
     }
     
+    // Devuelve una lista con los mensajes que ha recibido un usuario
+    public Collection<Mensaje> mensajesUsuario(int idUsuario){
+        Collection<Mensaje> mensajes;
+        Query consulta = getEntityManager().createQuery("SELECT m FROM Mensaje m WHERE m.idReceptor = :id");
+        consulta.setParameter("id", idUsuario);
+        mensajes = consulta.getResultList();
+        return mensajes;
+    }
+    
+    // Cantidad de mensajes sin leer que tiene un usuario
+    public int mensajesSinLeer(int idUsuario){
+        int cantidad = 0;
+        
+        Query consulta = getEntityManager().createQuery("SELECT m FROM Mensaje m WHERE m.idReceptor = :id");
+        consulta.setParameter("id", idUsuario);
+        cantidad = consulta.getResultList().size();
+        return cantidad;
+    }
 }
