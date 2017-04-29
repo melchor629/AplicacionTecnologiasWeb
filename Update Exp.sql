c97d4279a7c57cfd264a2801db71baea50c91b34
xEDG <eduardogj96@gmail.com>
2017-04-29 14:13:28 +0200

    He subido un SQL para añadir datos a la tabla experiencia laboral. De esta forma funciona la nueva consulta que he hecho en UsuarioFacade para buscar.

---
+++ Update Exp.sql
@@ -0,0 +1,14 @@
+INSERT INTO `tecnoweb`.`experiencialaboral`
+(`idUsuario`,
+`Fecha_Comienzo`,
+`Fecha_Finalizacion`,
+`Empresa`,
+`Puesto`,
+`Web_Empresa`)
+VALUES
+(1,2,4,5,6,7),
+(null,null,null,null,null,null),
+(null,null,null,null,null,null),
+(null,"Patos SA","Google",null,null,null),
+(null,null,null,null,null,null),
+(null,null,null,null,null,null);
