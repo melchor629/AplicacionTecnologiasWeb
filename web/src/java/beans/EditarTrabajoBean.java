c2d1167ef43ecdd7562250ea553e0dbbdd5811e9
francis1196 <francis8rs@gmail.com>
2017-05-30 19:48:57 +0200

    editarPerfil.xhtml y EditarPerfilBean Terminado Empezando editarTrabajo.xhtml y EditarTrabajoBean

---
+++ web/src/java/beans/EditarTrabajoBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Lucia y Francis
+ */
+@Named
+@RequestScoped
+public class EditarTrabajoBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    
+}
