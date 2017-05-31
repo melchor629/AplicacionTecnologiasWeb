96b376c9d229f8afd57913134c5d4b69ba0b66fc
francis1196 <francis8rs@gmail.com>
2017-05-31 17:41:43 +0200

    editarTrabajo.xhtml y EditarTrabajoBean Terminado

--- web/src/java/beans/EditarTrabajoBean.java
+++ web/src/java/beans/EditarTrabajoBean.java
@@ -5,9 +5,16 @@
  */
 package beans;
 
+import app.ejb.ExperienciaLaboralFacade;
+import app.ejb.UsuarioFacade;
+import app.entity.ExperienciaLaboral;
+import app.entity.Usuario;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -20,6 +27,77 @@ public class EditarTrabajoBean {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    @EJB
+    UsuarioFacade fachadaUsuario;
     
+    @EJB
+    ExperienciaLaboralFacade fachadaTrabajo;
     
+    @Inject SesionBean sesionBean;
+    
+    //DESCOMENTAR CUANDO ESTE CREADO
+    //@Inject PerfilBean perfilBean;
+    
+    Usuario usuario;
+    ExperienciaLaboral trabajo,trabajoOriginal;
+    String error=null;
+    
+    public void EditarTrabajoBean(){
+        
+    }
+    
+    //obtengo el usuario de sesion
+    @PostConstruct
+    public void init () {
+        this.usuario = this.sesionBean.obtenerUsuario();
+        
+        
+        //trabajo de prueba BORRAR
+        this.trabajo= fachadaTrabajo.obtenerTrabajos(usuario).get(0);
+        this.trabajoOriginal=fachadaTrabajo.obtenerTrabajos(usuario).get(0);;
+        
+       // this.trabajo = this.perfilBean.getTrabajoSeleccionado();
+       //this.trabajoOriginal=trabajo;
+    }
+    
+    public String getError(){
+        return error;
+    }
+    
+    public void setError(String error){
+        this.error=error;
+    }
+    
+    public ExperienciaLaboral getTrabajo(){
+        return trabajo;
+    }
+    
+    public void setTrabajo(ExperienciaLaboral trabajo){
+        this.trabajo=trabajo;
+    }
+    
+    public String doGuardar(){
+        
+        //si hay un error
+        if(error!=null){
+            
+            error="Ha ocurrido un Error";
+            return "editarTrabajo";
+        }else{
+            
+            error=null;
+            
+            //como no puedo editar el trabajo creo borro el que tengo y creo uno nuevo
+            
+            fachadaTrabajo.borrarExperienciaLaboral(this.usuario.getId(), this.trabajoOriginal.getExperienciaLaboralPK().getFechaComienzo());
+            usuario.getExperienciaLaboralCollection().remove(this.trabajoOriginal);
+            usuario.getExperienciaLaboralCollection().add(this.trabajo);
+            
+           fachadaUsuario.edit(this.usuario);
+            return "perfil";
+        }
+        
+        
+    }
+            
 }
